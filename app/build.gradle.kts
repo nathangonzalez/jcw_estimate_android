cb9744f727c588ffadd630de9300f91e5558397f
nathangonzalez <nathangonzalez12@gmail.com>
2025-08-15 16:48:55 +0000

    First build: Firebase config & Gradle; remove large artifacts and add .gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation(platform("com.google.firebase:firebase-bom:34.1.0"))
+    implementation("com.google.firebase:firebase-analytics")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-}
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,9 @@ plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
+    plugins {
+    id("com.android.application") version "8.1.0" apply false
+    kotlin("android") version "1.9.22" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
 }
+
